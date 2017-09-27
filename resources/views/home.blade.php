885f1cbae5fd65955e2c8fcaec4cff5e8aaf2793
Joshua Leung <joshuajazleung@gmail.com>
2017-09-27 15:35:09 -0700

    starter project init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Madison Marketing</title>
+
+        <link rel="stylesheet" href="{{ url(mix('/css/app.css')) }}">
+    </head>
+    <body>
+
+    </body>
+</html>
